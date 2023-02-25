28282bdb1042538f07425d3087228162fa0bf21e
Mário Oliveira <oiramlopes@hotmail.com>
2023-02-24 21:57:56 -0300

    Exercício metódos

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,34 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("Volume Atual: " + smartTv.volume);
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.diminuirCanal();
+        System.out.println("Canal Atual: " + smartTv.canal);
+
+  
+        System.out.println("TV Ligada?: " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+        
+        smartTv.ligar();
+        System.out.println("Novo status -> TV Ligada?: " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo status -> TV Ligada?: " + smartTv.ligada);
+
+    }
+}
